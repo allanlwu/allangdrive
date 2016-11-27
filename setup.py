1249e92d4a7d88e07e76b69390c634198f724f00
Allan Wu <allanlwu@gmail.com>
2016-11-27 12:17:53 -0800

    Changed URL

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="allangdrive",
     version='0.1.0',
-    url="https://github.com/allanlwu/allanresuse",
+    url="https://github.com/allanlwu/allangdrive",
     author="Allan Wu",
     description="Jupyter extension to allow user to sync files to Google Drive",
     packages=setuptools.find_packages(),
